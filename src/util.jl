f900fefc13aacb2b11e8c2ccff21f9ec1f78d8bd
David Hanak <dhanak@gmail.com>
2022-10-28 17:43:31 +0200

    Update hypergeometric implementation from numpy.random
    The hypergeometric distribution function was rewritten in numpy v1.24
    since it was copied here, and the new implementation fixes some issues
    with it. This change mirrors the Oct 1, 2021 version of
    random_hypergeometric.c.
    
    See more details in the related numpy change here:
    https://github.com/numpy/numpy/commit/b2d2b677c26c8da7052bbc653b20ad9717f078fe

--- src/util.jl
+++ src/util.jl
@@ -1,4 +1,3 @@
-
 # written by Poom Chiarawongse <eight1911@gmail.com>
 
 module util
@@ -186,7 +185,6 @@ module util
         return v
     end
 
-
     # The code function below is a small port from numpy's library
     # library which is distributed under the 3-Clause BSD license.
     # The rest of DecisionTree.jl is released under the MIT license.
@@ -195,105 +193,218 @@ module util
 
     # this is the code for efficient generation
     # of hypergeometric random variables ported from numpy.random
+
+    const logfact = @. Float64(log(factorial(big(0:125))))
+    const halfln2pi = log(2π) / 2
+    const D1 = 2 * sqrt(2 / MathConstants.e)
+    const D2 = 3 - 2 * sqrt(3 / MathConstants.e)
+
     function hypergeometric(good, bad, sample, rng)
 
-        @inline function loggam(x)
-            x0 = x
-            n = 0
-            if (x == 1.0 || x == 2.0)
-                return 0.0
-            elseif x <= 7.0
-                n = Int(floor(7 - x))
-                x0 = x + n
-            end
-            x2 = 1.0 / (x0*x0)
-            xp = 6.2831853071795864769252867665590 # Tau
-            gl0 = -1.39243221690590e+00
-            gl0 = gl0 * x2 + 1.796443723688307e-01
-            gl0 = gl0 * x2 - 2.955065359477124e-02
-            gl0 = gl0 * x2 + 6.410256410256410e-03
-            gl0 = gl0 * x2 - 1.917526917526918e-03
-            gl0 = gl0 * x2 + 8.417508417508418e-04
-            gl0 = gl0 * x2 - 5.952380952380952e-04
-            gl0 = gl0 * x2 + 7.936507936507937e-04
-            gl0 = gl0 * x2 - 2.777777777777778e-03
-            gl0 = gl0 * x2 + 8.333333333333333e-02
-            gl = gl0/x0 + 0.5*log(xp) + (x0-0.5)*log(x0) - x0
-            if x <= 7.0
-                @simd for k in 1:n
-                    gl -= log(x0 - k)
-                end
-            end
-            return gl
+        @inline function logfactorial(k)
+            # Use the lookup table.
+            k < length(logfact) && return logfact[k + 1]
+            # Use the Stirling series, truncated at the 1/k**3 term.
+            # (In a Python implementation of this approximation, the result
+            # was within 2 ULP of the best 64 bit floating point value for
+            # k up to 10000000.)
+            return (k + 0.5)*log(k) - k +
+                (halfln2pi + (1.0/k)*(1/12.0 - 1/(360.0*k*k)))
         end
 
-        @inline function hypergeometric_hyp(good, bad, sample)
-            d1 = bad + good - sample
-            d2 = min(bad, good)
+        #  Generate a sample from the hypergeometric distribution.
+        #
+        #  Assume sample is not greater than half the total.  See below
+        #  for how the opposite case is handled.
+        #
+        #  We initialize the following:
+        #      computed_sample = sample
+        #      remaining_good = good
+        #      remaining_total = good + bad
+        #
+        #  In the loop:
+        #  * computed_sample counts down to 0;
+        #  * remaining_good is the number of good choices not selected yet;
+        #  * remaining_total is the total number of choices not selected yet.
+        #
+        #  In the loop, we select items by choosing a random integer in
+        #  the interval [0, remaining_total), and if the value is less
+        #  than remaining_good, it means we have selected a good one,
+        #  so remaining_good is decremented.  Then, regardless of that
+        #  result, computed_sample is decremented.  The loop continues
+        #  until either computed_sample is 0, remaining_good is 0, or
+        #  remaining_total == remaining_good.  In the latter case, it
+        #  means there are only good choices left, so we can stop the
+        #  loop early and select what is left of computed_sample from
+        #  the good choices (i.e. decrease remaining_good by computed_sample).
+        #
+        #  When the loop exits, the actual number of good choices is
+        #  good - remaining_good.
+        #
+        #  If sample is more than half the total, then initially we set
+        #      computed_sample = total - sample
+        #  and at the end we return remaining_good (i.e. the loop in effect
+        #  selects the complement of the result).
+        #
+        #  It is assumed that when this function is called:
+        #    * good, bad and sample are nonnegative;
+        #    * the sum good+bad will not result in overflow;
+        #    * sample <= good+bad.
+        @inline function hypergeometric_sample(good, bad, sample)
+            total = good + bad
+            computed_sample = min(sample, total - sample)
+            remaining_total = total
+            remaining_good = good
 
-            Y = d2
-            K = sample
-            while Y > 0
-                Y -= floor(UInt, rand(rng) + Y/(d1 + K))
-                K -= 1
-                if K == 0
+            while computed_sample > 0 &&
+                remaining_good > 0 &&
+                remaining_total > remaining_good
+                if rand(rng, 0:remaining_total - 1) < remaining_good
+                    # Selected a "good" one, so decrement remaining_good.
+                    remaining_good -= 1
+                end
+                computed_sample -= 1
+            end
+
+         if remaining_total == remaining_good
+             # Only "good" choices are left.
+             remaining_good -= computed_sample
+         end
+
+            return if sample > total/2
+                remaining_good
+            else
+                good - remaining_good
+            end
+        end
+
+        # Generate variates from the hypergeometric distribution
+        # using the ratio-of-uniforms method.
+        #
+        # In the code, the variable names a, b, c, g, h, m, p, q, K, T,
+        # U and X match the names used in "Algorithm HRUA" beginning on
+        # page 82 of Stadlober's 1989 thesis.
+        #
+        # It is assumed that when this function is called:
+        #   * good, bad and sample are nonnegative;
+        #   * the sum good+bad will not result in overflow;
+        #   * sample <= good+bad.
+        #
+        # References:
+        # -  Ernst Stadlober's thesis "Sampling from Poisson, Binomial and
+        #    Hypergeometric Distributions: Ratio of Uniforms as a Simple and
+        #    Fast Alternative" (1989)
+        # -  Ernst Stadlober, "The ratio of uniforms approach for generating
+        #    discrete random variates", Journal of Computational and Applied
+        #    Mathematics, 31, pp. 181-189 (1990).
+        @inline function hypergeometric_hrua(good, bad, sample)
+            popsize = good + bad
+            computed_sample = min(sample, popsize - sample)
+            mingoodbad = min(good, bad)
+            maxgoodbad = max(good, bad)
+
+            # Variables that do not match Stadlober (1989)
+            #   Here               Stadlober
+            #   ----------------   ---------
+            #   mingoodbad            M
+            #   popsize               N
+            #   computed_sample       n
+
+            p = mingoodbad / popsize
+            q = maxgoodbad / popsize
+
+            #  mu is the mean of the distribution.
+            mu = computed_sample * p
+            a = mu + 0.5
+
+            # var is the variance of the distribution.
+            var = (popsize - computed_sample) *
+                computed_sample * p * q / (popsize - 1)
+            c = sqrt(var + 0.5)
+
+            # h is 2*s_hat (See Stadlober's thesis (1989), Eq. (5.17); or
+            # Stadlober (1990), Eq. 8).  s_hat is the scale of the "table mountain"
+            # function that dominates the scaled hypergeometric PMF ("scaled" means
+            # normalized to have a maximum value of 1).
+
+            h = D1*c + D2
+            m = floor(Int64, (computed_sample + 1) * (mingoodbad + 1) /
+                (popsize + 2))
+            g = logfactorial(m) +
+                logfactorial(mingoodbad - m) +
+                logfactorial(computed_sample - m) +
+                logfactorial(maxgoodbad - computed_sample + m)
+
+            # b is the upper bound for random samples:
+            # ... min(computed_sample, mingoodbad) + 1 is the length of the support.
+            # ... floor(a + 16*c) is 16 standard deviations beyond the mean.
+            #
+            # The idea behind the second upper bound is that values that far out in
+            # the tail have negligible probabilities.
+            #
+            # There is a comment in a previous version of this algorithm that says
+            #     "16 for 16-decimal-digit precision in D1 and D2",
+            # but there is no documented justification for this value.  A lower value
+            # might work just as well, but I've kept the value 16 here.
+
+            b = min(min(computed_sample, mingoodbad) + 1, floor(a + 16*c))
+
+            while true
+                U = rand(rng)
+                V = rand(rng) # "U star" in Stadlober (1989)
+                X = a + h*(V - 0.5) / U
+
+                # fast rejection:
+                if X < 0.0 || X >= b
+                    continue
+                end
+
+                K = floor(Int64, X)
+                gp = logfactorial(K) +
+                    logfactorial(mingoodbad - K) +
+                    logfactorial(computed_sample - K) +
+                    logfactorial(maxgoodbad - computed_sample + K)
+                T = g - gp
+
+                # fast acceptance:
+                if (U*(4.0 - U) - 3.0) <= T
+                    break
+                end
+
+                # fast rejection:
+                if U*(U - T) >= 1
+                    continue
+                end
+
+                if 2.0*log(U) <= T
+                    # acceptance
                     break
                 end
             end
 
-            Z = d2 - Y
-            return if good > bad
-                sample - Z
-            else
-                Z
-            end
-        end
-
-        @inline function hypergeometric_hrua(good, bad, sample)
-            mingoodbad = min(good, bad)
-            maxgoodbad = max(good, bad)
-            popsize = good + bad
-            m = min(sample, popsize - sample)
-            d4 = mingoodbad / popsize
-            d5 = 1.0 - d4
-            d6 = m*d4 + 0.5
-            d7 = sqrt((popsize - m) * sample * d4 * d5 / (popsize - 1) + 0.5)
-            # d8 = 2*sqrt(2/e) * d7 + (3 - 2*sqrt(3/e))
-            d8 = 1.7155277699214135*d7 + 0.8989161620588988
-            d9 = floor(UInt, (m + 1) * (mingoodbad + 1) / (popsize + 2))
-            d10 = (loggam(d9+1) + loggam(mingoodbad-d9+1) + loggam(m-d9+1) +
-                   loggam(maxgoodbad-m+d9+1))
-            d11 = min(m+1, mingoodbad+1, floor(UInt, d6+16*d7))
-
-            while true
-                X = rand(rng)
-                Y = rand(rng)
-                W = d6 + d8*(Y - 0.5)/X
-
-                (W < 0.0 || W >= d11) && continue
-                Z = floor(Int, W)
-                T = d10 - (loggam(Z+1) + loggam(mingoodbad-Z+1) + loggam(m-Z+1) +
-                           loggam(maxgoodbad-m+Z+1))
-                (X*(4.0-X)-3.0) <= T && break
-                (X*(X-T) >= 1) && continue
-                (2.0*log(X) <= T) && break
-            end
-
             if good > bad
-                Z = m - Z
+                K = computed_sample - K
             end
 
-            return if m < sample
-                good - Z
-            else
-                Z
+            if computed_sample < sample
+                K = good - K
             end
+
+            return K
         end
 
-        return if sample > 10
+        # Draw a sample from the hypergeometric distribution.
+        #
+        # It is assumed that when this function is called:
+        #   * good, bad and sample are nonnegative;
+        #   * the sum good+bad will not result in overflow;
+        #   * sample <= good+bad.
+        return if sample >= 10 && sample <= good + bad - 10
+            # This will use the ratio-of-uniforms method.
             hypergeometric_hrua(good, bad, sample)
         else
-            hypergeometric_hyp(good, bad, sample)
+            # The simpler implementation is faster for small samples.
+            hypergeometric_sample(good, bad, sample)
         end
     end
 
